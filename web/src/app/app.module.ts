62b76de2ffe238b82d0e013af1ff977184610a79
solenn <vosport46@gmail.com>
2021-03-09 14:57:07 +0100

    ajout component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActualiteComponent } from './pages/actualite/actualite.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { UserComponent } from './pages/user/user.component';
+import { InscriptionComponent } from './pages/user/inscription/inscription.component';
+import { ConnexionComponent } from './pages/user/connexion/connexion.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DescriptionComponent } from './pages/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActualiteComponent,
+    HeaderComponent,
+    UserComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    HomeComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
